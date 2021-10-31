6f323fc01462b4f940dbd564d0e5b3a371d25d65
Rashedul Islame <rasheduzzaman521@gmail.comm>
2021-10-31 19:10:23 +0600

    done

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    apiKey: "AIzaSyBPF4kXdwzVkNzifM31pmwz2p2kxYswt70",
+    authDomain: "sunny-travel-7d.firebaseapp.com",
+    projectId: "sunny-travel-7d",
+    storageBucket: "sunny-travel-7d.appspot.com",
+    messagingSenderId: "791652803700",
+    appId: "1:791652803700:web:60b08670099d646e13dd21"
   };
 export default firebaseConfig;
