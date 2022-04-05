4dec227d6dfae9bbc3a1811b83b7165afefa3e58
Rashedul-4-Islam <rasheduzzaman521@gmail.com>
2022-04-05 23:42:59 +0600

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,20 +7,22 @@ const Footer = () => {
             <div className="row bg-dark text-light mt-5 pt-4">
                 <div className="row">
                     <div className="col-6 col-md-3">
-                        <h3 className="text-warning fw-bold">SUNNY TRAVEL</h3>
-                        <p>Our travel agency is a private retailer or public service that provides travel and tourism-related services.</p>
-                        <p><i className="fas fa-envelope me-2"></i> sunny@travel.com</p>
-                        <p><i className="fas fa-phone-alt me-2"></i> +8801574832566</p>
-                        <p><i className="fas fa-map-marker-alt me-2"></i> Gulshan-2,Dhaka,BD</p>
+                        <h1 className="text-warning fw-bold">SUNNY TRAVEL</h1>
+                         <div className='fs-5'>
+                            <p>Our travel agency is a private retailer or public service that provides travel and tourism-related services.</p>
+                            <p><i className="fas fa-envelope me-2"></i> sunny@travel.com</p>
+                            <p><i className="fas fa-phone-alt me-2"></i> +8801574832566</p>
+                            <p><i className="fas fa-map-marker-alt me-2"></i> Gulshan-2,Dhaka,BD</p>
+                         </div>
                     </div>
                     <div className="col-6 col-md-3">
-                        <h3>Our Recent Posts</h3>
+                        <h2>Our Recent Posts</h2>
                         <h5 className="text-success mt-4">Visit Thailand, Bali And China <br /> <span className="fs-6 text-light">12 September,2019</span></h5>
                         <h5 className="text-success">The Sound Of Our Jungle <br /> <span className="fs-6 text-light">23 March,2021</span></h5>
                         <h5 className="text-success">New Year, New Resolutions! <br /> <span className="fs-6 text-light">1 january,2021</span></h5>
                     </div>
                     <div className="col-6 col-md-3">
-                        <h5>Subscribe to our Newsletter</h5>
+                        <h3>Subscribe to our Newsletter</h3>
                         <form className="ps-5">
                             <input  className="form-control w-75 mt-3" type="text" placeholder="name" />
                             <input className="form-control w-75 mt-2" type="email" placeholder="email" />
